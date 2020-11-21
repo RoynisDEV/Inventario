6b0649d6243ad8dc0564dbb594a2d2afa957b096
RoynisDEV <enriqbravo@hotmail.com>
2020-11-21 15:14:21 -0500

    creacion de render de las vistas

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 require('./database');
 
 
-
+// min 52:51
 
 
 //settings
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 // configuracion de la rutas de la vistas 
 router.get('/user/signin', (req,res)=>{
-    res.send('Ingresando a la app');
+    res.render('user/ingresar.hbs');
 });
 
 router.get('/user/registro', (req,res)=>{
-    res.send('formulario de registro');
+    res.render('user/registro.hbs');
 });
 
 
